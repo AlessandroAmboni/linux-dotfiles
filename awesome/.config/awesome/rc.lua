99cf30f3eb79910efb885de05306a6fb04ec419d
AlessandroAmboni <alessandroamboni01@gmail.com>
2024-08-20 22:10:28 -0300

    added picom to startup

--- awesome/.config/awesome/rc.lua
+++ awesome/.config/awesome/rc.lua
@@ -574,3 +574,8 @@ end)
 client.connect_signal("focus", function(c) c.border_color = beautiful.border_focus end)
 client.connect_signal("unfocus", function(c) c.border_color = beautiful.border_normal end)
 -- }}}
+
+
+-- statup programs
+
+awful.util.spawn('picom')
